b048ce46a35a22b303325e70851efef5d7495ffe
qkzk <qkzk@users.noreply.github.com>
2023-12-05 15:22:56 +0100

    Add files via upload

--- ex_flask_bdd.py
+++ ex_flask_bdd.py
@@ -5,24 +5,26 @@ query = "SELECT * FROM `exempe`"
 app = Flask(__name__)
 
 class Personne:
-	def __init__(self, tup: tuple):
-		self.__id = tup[0]
-		self.__nom = tup[1]
+    def __init__(self, tup: tuple):
+        self.__id = tup[0]
+        self.__nom = tup[1]
 
-	def format(self):
-		return self.__nom
+    def format(self):
+        return self.__nom
 
-	def __repr__(self):
-		return self.format()
+    def __repr__(self):
+        return self.format()
 
 @app.route("/")
 def index():
-	conn = sqlite3.connect("ex.bdd.db")
-	curr = conn.cursor()
-	curr.execute(query)
-	noms = curr.fetchall()
-	personnes = [Personne(nom) for nom in noms]
-	print(personnes)
-	return render_template("index.html", personnes=personnes)
+    conn = sqlite3.connect("ex.bdd.db")
+    curr = conn.cursor()
+    curr.execute(query)
+    noms = curr.fetchall()
+    curr.close()
+    conn.close()
+    personnes = [Personne(nom) for nom in noms]
+    print(personnes)
+    return render_template("index.html", personnes=personnes)
 
 app.run(debug=True, port=8888)
